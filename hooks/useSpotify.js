8dffea79ab1a80b33fc6473773ef9928c3504883
baron <baronchibuike@gmail.com>
2021-12-15 18:07:34 +0100

    fixing bug

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,15 +1,21 @@
 import { useSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
-import spotifyApi from "../lib/spotifyutils";
+import SpotifyWebApi from "spotify-web-api-node";
 
 function useSpotify() {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  });
   const { data: session, status } = useSession();
   useEffect(() => {
     if (session) {
       // if refresh access token attempt fails, direct user to login...
       if (session.error === "RefreshAccessTokenError") {
+        console.log("error occured in spotify hook", session.error);
         signIn();
       }
+      console.log(session.user.accessToken, "<<<<<<<<<<<Access token>>>>>>");
       spotifyApi.setAccessToken(session.user.accessToken);
     }
   }, [session]);
